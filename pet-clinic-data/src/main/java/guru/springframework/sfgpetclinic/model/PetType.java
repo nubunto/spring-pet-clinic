5e106b490226ef7843db315bbee5433a20897ea6
Bruno Panuto <bruno.panuto@ifood.com.br>
2019-01-02 00:46:57 -0200

    initial commit

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class PetType {
+    private String name;
+
+    public PetType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
